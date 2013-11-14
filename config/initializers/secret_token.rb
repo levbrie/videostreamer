f860f1ab47710430abb7095c2ba6d7c5c4a28269
Lev Brie <lev.brie@gmail.com>
2013-11-14 03:41:39 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Videostreamer::Application.config.secret_key_base = 'e595862574ce1c94e69ffc6d68ec51216890f6df335886f2ffb5b0985d19061201a37018d8a6444258b1475fc9b936e49fb65d6095e93bd6a2ca823c9bf95176'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Videostreamer::Application.config.session_store :cookie_store, key: '_videostreamer_session'
